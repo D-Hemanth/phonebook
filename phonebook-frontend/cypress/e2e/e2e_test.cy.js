b1f6eabbe20df1df380bbb2cf96ac79d4a5d74bf
Hemanth-D <hemanthh531@gmail.com>
2022-10-29 20:52:40 +0530

    test build working fine

--- phonebook-frontend/cypress/e2e/e2e_test.cy.js
+++
@@ -1,7 +0,0 @@
-describe('Phonebook', function () {
-  it('front page can be opened', function () {
-    cy.visit('http://localhost:3000')
-    cy.contains('Numbers')
-    cy.contains('filter shown with')
-  })
-})
